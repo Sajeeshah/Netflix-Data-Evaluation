03e3a9301c7ab3b0b43691df24cdd6c27663cb74
Sajee Shah <shah.sajee2004@gmail.com>
2025-07-30 01:00:30 +0530

    Updated Business Problems file

--- Business Problems.sql
+++ Business Problems.sql
@@ -78,6 +78,81 @@ WHERE
     AND split_part(duration, ' ', 1)::int > 5;
 
 
+--count the no of content in each genre
+select * from netflix
+
+
+select 
+	unnest(string_to_array(listed_in,',')) as genre,
+	count(*)
+from netflix
+group by genre;
+
+--find each year and the average no of content released on netflix in india
+select 
+	extract(year from to_date(date_added, 'Month DD,YYYY')) as year,
+	count(*) as yearly_content,
+	round (
+	count(*) :: numeric / (select count(*) from netflix where country = 'India') :: integer * 100
+	,2)
+	as average
+from netflix
+where country ilike '%india%'
+group by 1
+
+
+--list all movies that are documentaries
+select 
+	*
+from netflix
+where type = 'Movie'
+and listed_in ilike '%documentaries%'
+
+
+--find all the content without a director
+select 
+	*
+from netflix
+where director is null
+
+--find all movies that salman khan has appeared in last 10 years
+SELECT * 
+FROM netflix
+WHERE casts LIKE '%Salman Khan%'
+  AND release_year > EXTRACT(YEAR FROM CURRENT_DATE) - 10;
+
+
+--find the top 10 actors who appeared in the most movies produced in india
+select
+	unnest(string_to_array(casts, ',')),
+	count(*)
+from netflix
+where country = 'India'
+group by 1
+order by count(*) desc
+limit 10
+
+-- Categorize content as 'Bad' if it contains 'kill' or 'violence' and 'Good' otherwise. Count the number of items in each category.
+with new_table 
+as
+(
+select 
+	*,
+	case
+		when
+		description ilike '%kill%'
+		or 
+		description ilike '%violence%' then 'bad_content'
+		else 'good_content'
+	end category
+from netflix
+)
+
+select category,
+count(*)
+from new_table
+group by 1
+
 
 
 
